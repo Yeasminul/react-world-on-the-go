c4e62e681564cadd3bda732260e099c52cb5aad5
Yeasminul <yeasminul18@gmail.com>
2025-10-05 07:17:42 +0600

    this is anonther commit

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
-const Country = ({country}) => {
+const Country = ({country,handleVisitedCountry}) => {
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = ()=>{
+        //  if(visited) {
+        //   setVisited(false)
+        // 
+        // }
+        // else{
+        // setVisited(true)
+        //  }
+        // setVisited(visited ? false :true);
+        setVisited(!visited);
+        handleVisitedCountry(country);
+    }
     
     return (
-        <div className='country'>
+        <div className= {`country ${visited && 'country-visited'}`} >
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
-            <h3>Population: {country.population.population}</h3>
-            <h3>Area: {country.area.area}</h3>
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"} </p>
+
+            <button onClick={handleVisited}> {visited ? 'visited' : 'not visited'} </button>
         </div>
     );
 };
